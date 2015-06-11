14fa93177661ba5015552a07d7f4ec59a1d9dc5d
Ibrahim-Keyrouz <ibrahim_keyrouz@hotmail.com>
2015-06-11 09:09:11 +0300

    Adding the function httpPost to call the create(POST) method in a web service

--- Achat/Application/ScanQR/gen/com/example/scanqr/R.java
+++ Achat/Application/ScanQR/gen/com/example/scanqr/R.java
@@ -15,6 +15,7 @@ public final class R {
     }
     public static final class id {
         public static final int bScan=0x7f060000;
+        public static final int button1=0x7f060002;
         public static final int etBarcode=0x7f060001;
     }
     public static final class layout {
--- Achat/Application/ScanQR/src/com/example/scanqr/MainActivity.java
+++ Achat/Application/ScanQR/src/com/example/scanqr/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.scanqr;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,6 +23,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements OnClickListener {
 	static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
 	Button bscan;
+	Button b1;
 	EditText etBarcode;
 
 	@Override
@@ -32,7 +38,8 @@ public class MainActivity extends Activity implements OnClickListener {
 		bscan = (Button) findViewById(R.id.bScan);
 		etBarcode = (EditText) findViewById(R.id.etBarcode);
 		bscan.setOnClickListener(this);
-
+		b1 = (Button) findViewById(R.id.button1);
+		b1.setOnClickListener(this);
 	}
 
 	public void scanBar(View v) {
@@ -126,8 +133,48 @@ public class MainActivity extends Activity implements OnClickListener {
 		case R.id.bScan :
 			scanBar(v);
 			break;
+			
+		case R.id.button1 :
+	
+			new LongRunningGetIO().execute(); 
+			break;
 		}
 
 	}
+	
+	
+	
+	class LongRunningGetIO extends  AsyncTask <Void, Void, String>{
+
+		
+
+		@Override
+		protected String doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			
+			HttpClient httpClient = new DefaultHttpClient();
+			try {
+			    HttpPost request = new HttpPost("http://192.168.1.110:8080/CarsWS/webresources/entities.cars");
+			    StringEntity params1 =new StringEntity("details={\"brand\":\"myname\",\"info\":\"20\",\"year\":1990} ");
+			    request.addHeader("content-type", "application/json");
+			    request.setEntity(params1);
+			    HttpResponse response = httpClient.execute(request);
+
+			    // handle response here...
+			}catch (Exception ex) {
+			    // handle exception here
+				Toast.makeText(getBaseContext(), ex.toString(), Toast.LENGTH_SHORT).show();
+			} finally {
+			    httpClient.getConnectionManager().shutdown();
+			}
+			return null;
+		}
+		
+		
+		
+		
+
+	}
+
 
 }
