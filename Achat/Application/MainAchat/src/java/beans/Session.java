dc1559c3bc7fc9207944b91d283ec0439a1565fd
Ibrahim-Keyrouz <ibrahim_keyrouz@hotmail.com>
2015-07-19 18:07:15 +0300

    Adding the welcome on the template and the logout

--- Achat/Application/MainAchat/src/java/beans/Session.java
+++ Achat/Application/MainAchat/src/java/beans/Session.java
@@ -5,21 +5,16 @@
  */
 package beans;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.faces.application.ConfigurableNavigationHandler;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 //import static org.apache.poi.hssf.usermodel.HeaderFooter.page;
@@ -61,80 +56,13 @@ public class Session implements Serializable {
         this.password = password;
     }
 
-    public String login() throws ServletException {
-
-        if (this.userid.isEmpty()) {
-            FacesMessage message = new FacesMessage("Please enter a user !!!");
-            message.setSeverity(FacesMessage.SEVERITY_ERROR);
-
-            FacesContext.getCurrentInstance().addMessage("form-login:usernameInput", message);
-
-            return null;
-        }
-
-        if (this.password.isEmpty()) {
-            FacesMessage message = new FacesMessage("Please enter a password !!!");
-            message.setSeverity(FacesMessage.SEVERITY_ERROR);
-            FacesContext.getCurrentInstance().addMessage("form-login:usernameInput", message);
-            return null;
-        }
-
-        //context.addMessage(null,new FacesMessage("Ok"));
-        FacesContext context = FacesContext.getCurrentInstance();
-        request = (HttpServletRequest) context.getExternalContext().getRequest();
-        try {
-            request.login(this.userid, this.password);
-        } catch (ServletException e) {
-            System.out.println(e);
-            FacesMessage message = new FacesMessage("Login Failed !!!");
-            message.setSeverity(FacesMessage.SEVERITY_ERROR);
-
-            FacesContext.getCurrentInstance().addMessage("form-login:usernameInput", message);
-
-            return null;
-        }
-
-        return "/products/List";
-
-    }
  
  public String logout() {
     HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
     session.invalidate();
     return "/Login?faces-redirect=true"; }
 
-    public void methodInManagedBean() throws IOException, ClassNotFoundException, SQLException {
-        int mmbrexist = 0;
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-        Connection connection = null;
-        connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "remoteusers");
-        Statement stmt = null;
-        String query = "select USERID  from users";
-        try {
-            stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                mmbrexist++;
-
-            }
-            // >
-            if (mmbrexist > 0) { 
-                nav.performNavigation("/Login.xhtml");
-            } else {
-                nav.performNavigation("/CreatingAdmin.xhtml");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (stmt != null) {
-                stmt.close();
-                connection.close();
-            }
-        }
-
-     //////////////////////////+
-    }
-
+   
     public void createAdmin() throws ClassNotFoundException, SQLException {
         try {
         this.setUserid("0001");
@@ -179,6 +107,14 @@ public class Session implements Serializable {
             
             }
         }
+     
+     public String getUser() {
+       // return request.getUserPrincipal().getName();
+         FacesContext context = FacesContext.getCurrentInstance();
+        request = (HttpServletRequest) context.getExternalContext().getRequest();
+        return request.getRemoteUser();
+        
+     }
      
  
 
